40766dd26a9309e5b4524a895e264b7f08651e65
HybridRbt <jianfeng.yang0120@gmail.com>
2019-04-25 16:58:13 -0500

    add two new classes

---
+++ TestMessenger/PendingMsg.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMessenger
+{
+    class PendingMsg
+    {
+        private byte[] msgBytes;
+
+        private int msgLength;
+    }
+}
